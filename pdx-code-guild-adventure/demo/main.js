178dd20255066455fd6b55f92fec500243465859
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-05-28 11:14:52 -0700

    fixed right input bug

--- pdx-code-guild-adventure/demo/main.js
+++ pdx-code-guild-adventure/demo/main.js
@@ -227,6 +227,7 @@ document.addEventListener('keyup', event => {
   if (event.key === 'ArrowUp') up = false
   if (event.key === 'ArrowDown') down = false
   if (event.key === 'ArrowLeft') left = false
+  if (event.key === 'ArrowRight') right = false
   if (event.key === 'q') q = false
 })
 
